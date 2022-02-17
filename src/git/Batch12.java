d2ea1df645c390ad61a3ee15d1c6857ca64fbc6d
mujeebnoorzad <mujeeb.noorzad@gmail.com>
2022-02-17 11:48:32 -0800

    i have update some code

--- src/git/Batch12.java
+++ src/git/Batch12.java
@@ -5,6 +5,7 @@ public class Batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.print("we are here");
+  System.out.print("we are here");
 	}
 
 }
